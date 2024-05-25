4fcbd1416472577b0d06d67f086d62ece5fafc20
ERROR2723 <90236730+ERROR2723@users.noreply.github.com>
2024-05-25 14:13:35 +0700

    put request 25/05/24

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.get('/api/users/', (req, res) => {
         return res.send(mockUser.filter((user) => user[filter].includes(value)))
             // return res.send(mockUser.filter((user) => user.name.includes(value)))
     }
-})
+});
 
 app.get('/api/users/:id', (req, res) => {
     console.log(req.params);
@@ -38,15 +38,29 @@ app.get('/api/users/:id', (req, res) => {
         return res.sendStatus(404);
     }
     return res.send(findUser);
-})
+});
 
 app.post('/api/users', (req, res) => {
     console.log(req.body);
     const { body } = req;
     const newUser = { id: mockUser[mockUser.length - 1].id + 1, ...body }
-    mockUser.push(newUser);
+    mockUser.push(newUser);;
     return res.status(201).send(newUser);
-})
+});
+
+app.put('/api/users/:id', (req, res) => {
+    const { body, params: { id } } = req;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+        return res.sendStatus(400);
+    }
+    const findUserIndex = mockUser.findIndex((user) => user.id === parsedId);
+    if (findUserIndex === -1) { return res.sendStatus(404); }
+    mockUser[findUserIndex] = { id: parsedId, ...body };
+    res.sendStatus(200);
+
+
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
