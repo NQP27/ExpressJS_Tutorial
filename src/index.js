fe2b682ed30108833d98a884e7ba6bc1237beab5
ERROR2723 <90236730+ERROR2723@users.noreply.github.com>
2024-05-25 12:43:01 +0700

    query param 25/05/24

--- src/index.js
+++ src/index.js
@@ -5,26 +5,22 @@ const port = 3000;
 const mockUser = [
     { id: 1, name: "Phuoc", add: "TB" },
     { id: 2, name: "Loc", add: "HN" },
-    { id: 3, name: "Nhi", add: "QN" }
+    { id: 3, name: "Nhi", add: "QN" },
+    { id: 4, name: "Ngan", add: "TB" },
+    { id: 5, name: "Ha", add: "HP" },
+    { id: 6, name: "Nam", add: "HY" },
 ];
 
-app.get('/api/user', (req, res) => {
-    res.send(mockUser);
-});
-
-app.get('/api/user/:id', (req, res) => {
-    console.log(req.params);
-    const parsedId = parseInt(req.params.id);
-    if (isNaN(parsedId)) {
-        return res.status(400).send({
-            msg: "Bad Request."
-        });
+app.get('/api/users/', (req, res) => {
+    console.log(req.query);
+    const {
+        query: { filter, value }
+    } = req;
+    if (!filter && !value) { return res.send(mockUser); }
+    if (filter && value) {
+        return res.send(mockUser.filter((user) => user[filter].includes(value)))
+            // return res.send(mockUser.filter((user) => user.name.includes(value)))
     }
-    const findUser = mockUser.find((user) => user.id === parsedId);
-    if (!findUser) {
-        return res.sendStatus(404);
-    }
-    return res.send(findUser);
 })
 
 app.listen(port, () => {
