0c3505db8b8a03b05d9fa54a1407d6155aa143fe
ERROR2723 <90236730+ERROR2723@users.noreply.github.com>
2024-05-26 14:28:31 +0700

    middleware 26/05/24

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.json());
-
 const mockUser = [
     { id: 1, name: "Phuoc", add: "TB" },
     { id: 2, name: "Loc", add: "HN" },
@@ -13,66 +11,12 @@ const mockUser = [
     { id: 6, name: "Nam", add: "HY" },
 ];
 
-app.get('/api/users/', (req, res) => {
-    console.log(req.query);
-    const {
-        query: { filter, value }
-    } = req;
-    if (!filter && !value) { return res.send(mockUser); }
-    if (filter && value) {
-        return res.send(mockUser.filter((user) => user[filter].includes(value)))
-            // return res.send(mockUser.filter((user) => user.name.includes(value)))
-    }
-});
+const loggingMiddleWare = (req, res, next) => {
+    console.log(`${req.method} - ${req.url}`);
+    next();
+};
 
-app.get('/api/users/:id', (req, res) => {
-    console.log(req.params);
-    const parsedId = parseInt(req.params.id);
-    if (isNaN(parsedId)) {
-        return res.status(400).send({
-            msg: "Bad Request."
-        });
-    }
-    const findUser = mockUser.find((user) => user.id === parsedId);
-    if (!findUser) {
-        return res.sendStatus(404);
-    }
-    return res.send(findUser);
-});
-
-app.post('/api/users', (req, res) => {
-    console.log(req.body);
-    const { body } = req;
-    const newUser = { id: mockUser[mockUser.length - 1].id + 1, ...body }
-    mockUser.push(newUser);;
-    return res.status(201).send(newUser);
-});
-
-app.put('/api/users/:id', (req, res) => {
-    const { body, params: { id } } = req;
-    const parsedId = parseInt(id);
-    if (isNaN(parsedId)) {
-        return res.sendStatus(400);
-    }
-    const findUserIndex = mockUser.findIndex((user) => user.id === parsedId);
-    if (findUserIndex === -1) { return res.sendStatus(404); }
-    mockUser[findUserIndex] = { id: parsedId, ...body };
-    return res.sendStatus(200);
-});
-
-app.patch('/api/users/:id', (req, res) => {
-    const { body, params: { id } } = req;
-    const parsedId = parseInt(id);
-    if (isNaN(parsedId)) {
-        return res.sendStatus(400);
-    }
-    const findUserIndex = mockUser.findIndex((user) => user.id === parsedId);
-    if (findUserIndex === -1) { return res.sendStatus(404); }
-    mockUser[findUserIndex] = {...mockUser[findUserIndex], ...body };
-    return res.sendStatus(200);
-});
-
-app.delete('/api/users/:id', (req, res) => {
+const handleIndexByUserId = (req, res, next) => {
     const { params: { id } } = req;
     const parsedId = parseInt(id);
     if (isNaN(parsedId)) {
@@ -80,6 +24,71 @@ app.delete('/api/users/:id', (req, res) => {
     }
     const findUserIndex = mockUser.findIndex((user) => user.id === parsedId);
     if (findUserIndex === -1) { return res.sendStatus(404); }
+    req.findUserIndex = findUserIndex;
+    next();
+};
+
+app.use(express.json());
+// app.use(loggingMiddleWare);
+
+app.get('/api/users/', (req, res) => {
+    res.send(mockUser);
+});
+
+// app.get('/api/users/', (req, res) => {
+//     console.log(req.query);
+//     const {
+//         query: { filter, value }
+//     } = req;
+//     if (!filter && !value) { return res.send(mockUser); }
+//     if (filter && value) {
+//         return res.send(mockUser.filter((user) => user[filter].includes(value)))
+//             // return res.send(mockUser.filter((user) => user.name.includes(value)))
+//     }
+// });
+
+app.get('/api/users/:id', (req, res) => {
+    console.log(req.params);
+    const { params: { id } } = req;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+        return res.sendStatus(400);
+    }
+    const findUser = mockUser.find((user) => user.id === parsedId);
+    if (findUser === -1) { return res.sendStatus(404); }
+    return res.send(findUser);
+});
+
+
+//Thêm 
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    const { body } = req;
+    const newUser = { id: mockUser[mockUser.length - 1].id + 1, ...body }
+    mockUser.push(newUser);
+    return res.status(201).send(newUser);
+});
+
+
+//Thay đổi toàn bộ thuộc tính của một bản ghi
+app.put('/api/users/:id', handleIndexByUserId, (req, res) => {
+    const { body, findUserIndex } = req;
+    mockUser[findUserIndex] = { id: mockUser[findUserIndex].id, ...body };
+    return res.sendStatus(200);
+});
+
+
+//Thay đổi một số thuộc tính của một bản ghi
+app.patch('/api/users/:id', handleIndexByUserId, (req, res) => {
+    const { body, findUserIndex } = req;
+    mockUser[findUserIndex] = {...mockUser[findUserIndex], ...body };
+    return res.sendStatus(200);
+});
+
+
+//Xóa
+app.delete('/api/users/:id', handleIndexByUserId, (req, res) => {
+    const { findUserIndex } = req;
     mockUser.splice(findUserIndex, 1);
     return res.sendStatus(200);
 });
